c8c161d08ccb707c07e6d7a595bffda8fc8de5d2
zdegroot <degroot@chapman.edu>
2026-09-08 02:53:39 -0700

    Removed extra comments

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,18 +7,13 @@ public class Game : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // show start screen
         CanvasGroupDisplayer.Show(CanvasGroup);
         
     }
 
     public void OnStartButtonClicked()
     {
-        print("clicked");
-        // hide start screen
         CanvasGroupDisplayer.Hide(CanvasGroup);
-        
-        // remember that the game has started
         isGameStarted = true;
     }
 }
