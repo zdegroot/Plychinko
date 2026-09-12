681acca3e3341c563fc9d7eed62cc77e82cb2464
zdegroot <degroot@chapman.edu>
2026-09-11 18:15:38 -0700

    Made ball launch in random directions

--- Assets/Scripts/BallDropper.cs
+++ Assets/Scripts/BallDropper.cs
@@ -20,7 +20,17 @@ public class BallDropper : MonoBehaviour
         Vector3 spawnPosition = GetSpawnPosition();
         
         // create a ball at that position
-        Instantiate(BallPrefab, spawnPosition, Quaternion.identity); // what to create, where to create it, and how to rotate
+        GameObject ball = Instantiate(BallPrefab, spawnPosition, Quaternion.identity);
+
+        AddRandomForce(ball);
+    }
+
+    private void AddRandomForce(GameObject ball)
+    {
+        Rigidbody2D rigidbody = ball.GetComponent<Rigidbody2D>();
+
+        float randomHorizontalForce = Random.Range(-5.5f, 5.5f);
+        rigidbody.AddForce(new Vector2(randomHorizontalForce, 0f), ForceMode2D.Impulse);
     }
 
     public Vector3 GetSpawnPosition()
